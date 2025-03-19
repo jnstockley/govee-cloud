426fa51b3c5da466fef5a27a38c3be44688086c0
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-19 15:57:47 -0500

    Remove Docker files

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,3 @@ uvx ruff format --check
 # Run Shell linter
 find . -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) -print0 | xargs -0 shellcheck -x -S style
 shfmt --diff -i 4 -ci .
-
-# Run YAML linter
-yamllint -s .
-npx dclint -r --fix --max-warnings 0 ./
