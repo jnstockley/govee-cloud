25ebbfa4f5e26a75dbd005fa1e233646feaa228d
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-19 18:45:02 -0500

    Update lint script to fix issues

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Run Python linter
-uvx ruff check
-uvx ruff format --check
+uvx ruff check --fix
+uvx ruff format
 
 # Run Shell linter
 find . -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) -print0 | xargs -0 shellcheck -x -S style
